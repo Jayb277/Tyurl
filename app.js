bc4483c11d01560437a6f0a9b825d31a8f41b901
jayb277 <jayasreesr77@gmail.com>
2025-10-26 23:49:17 +0530

    resolve

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const app = express();
 
 connectDB();
 
-import indexRouter from './routes/index.js';
+// import indexRouter from './routes/index.js';
 import urlsRouter from './routes/urls.js';
 
 // Body Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/api', urlsRouter);
 
 // Server Setup
